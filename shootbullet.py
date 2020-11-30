843386c42b7f38cc31b49cc984e4e02ba4b264f8
harri853 <59712756+harri853@users.noreply.github.com>
2020-11-30 00:10:55 +0000

    Update shootbullet.py
    adds a type of mob that spawn and move and random speeds towards the player continuously.
    will tidy it up later but will work fine for now for testing purposes.

--- shootbullet.py
+++ shootbullet.py
@@ -27,6 +27,25 @@ class Bullet(pygame.sprite.Sprite):
 
         if self.rect.x >= screen_width + 200: #if bullet goes too far to the right,
             self.kill() #bullet will destroy itself to save memory and improve performance
+           
+class Mob(pygame.sprite.Sprite): #spawn enemies
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,40))
+        self.image.fill((0,255,0))
+        self.rect = self.image.get_rect()
+        self.rect.y = random.randrange(screen_height - self.rect.height)#spanwns them on x axis outside of screen to the right
+        self.rect.x = random.randrange(700, screen_width)
+        self.speedx = random.randrange(1, 3)# randomise their speed
+
+    def update(self):
+        self.rect.x -= self.speedx #mobs go in left direction
+        if self.rect.x <= 0:
+            self.rect.y = random.randrange(screen_height - self.rect.height)  # spanwns them on x axis outside of screen to the right
+            self.rect.x = random.randrange(700, screen_width)  # spawn in a random place to the right of the screen
+            self.speedx = random.randrange(1, 8)  # randomise their speed
+#currently new mobs dont spanwn, in the futuer after a mob dies, new one will spawn. need to do collision mehcanic first
+
 
 # General setup
 pygame.init()
@@ -45,6 +64,14 @@ player_group.add(player)
 #bullet group
 bullet_group = pygame.sprite.Group()
 
+#mob group
+mob = pygame.sprite.Group()
+mob_number = 5
+for i in range(mob_number):#no of mobs
+    m = Mob()
+    mob.add(m)
+    #player.add(m)#might take this out
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -54,7 +81,9 @@ while True:
             bullet_group.add(player.create_bullet())
 
     screen.fill((30,30,30))
+    mob.update()
     player_group.update()
+    mob.draw(screen)
     bullet_group.draw(screen)
     player_group.draw(screen)
     bullet_group.update()
