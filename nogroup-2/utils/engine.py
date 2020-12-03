a1a4cad68c4a59bccd0a5cedb56a5713c0920fb1
harri853 <59712756+harri853@users.noreply.github.com>
2020-12-03 17:25:54 +0000

    Add files via upload

--- nogroup-2/utils/engine.py
+++ nogroup-2/utils/engine.py
@@ -16,13 +16,32 @@ import utils.hongyuan as FuncHongyuan
 import utils.peggy as FuncPeggy
 
 # Classes and Functions
+
+# Shutdown application
 def Shutdown():
     pygame.quit()
     sys.exit()
     os._exit()
+    
+def CenterWindow():
+    FuncAlex.CenterWindow()
 
+# Draw menu buttons - centered on screen (stackable)
 def DrawMenuButton(window, width, height, button_number, image):
     return FuncAlex.DrawMenuButton(window, width, height, button_number, image)
 
-def DrawMenuBackground(window, width, height, image):
-    FuncAlex.DrawMenuBackground(window, width, height, image)
+# Draw static background - useful for menu screens etc
+def DrawStaticBackground(window, width, height, image):
+    FuncAlex.DrawStaticBackground(window, width, height, image)
+
+    
+# Draw infinite scroll
+def DrawScrollBackground(window, width, speed, image, x):
+    return FuncAlex.DrawScrollBackground(window, width, speed, image, x)
+
+def setup_sprites(width, height):
+    return FuncHarri.setup_sprites(width, height)
+
+def draw_sprites(player_group,mob,bullet_group,player,screen):
+    return FuncHarri.draw_sprites(player_group,mob,bullet_group,player,screen)
+
--- nogroup-2/utils/harri.py
+++ nogroup-2/utils/harri.py
@@ -5,14 +5,14 @@ Created on Fri Nov 20 13:49:03 2020
 @author: Harri
 """
 # Import libraries and modules
-import pygame, sys
+import pygame, sys, random
 # Import individual contribuutions
 import utils.alex as FuncAlex
 import utils.hongyuan as FuncHongyuan
 import utils.peggy as FuncPeggy
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,screen_width, screen_height):
         super().__init__()
         self.image = pygame.Surface((100,100))#100pixels wide and high
         self.image.fill((255,255,255))
@@ -38,7 +38,65 @@ class Bullet(pygame.sprite.Sprite):
 
         if self.rect.x >= screen_width + 200: #if bullet goes too far to the right,
             self.kill() #bullet will destroy itself to save memory and improve performance
-           
+            
+class Mob(pygame.sprite.Sprite): #spawn enemies
+    def __init__(self, screen_width, screen_height):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,40))
+        self.image.fill((0,255,0))
+        self.rect = self.image.get_rect()
+        self.rect.y = random.randrange(screen_height - self.rect.height)#spanwns them on x axis outside of screen to the right
+        self.rect.x = random.randrange(700, screen_width)
+        self.speedx = random.randrange(1, 8)# randomise their speed
 
-   
-      
+    def update(self):
+        self.rect.x -= self.speedx #mobs go in left direction
+        if self.rect.x <= 0:
+            self.rect.y = random.randrange(screen_height - self.rect.height)  # spanwns them on x axis outside of screen to the right
+            self.rect.x = random.randrange(700, screen_width)  # spawn in a random place to the right of the screen
+            self.speedx = random.randrange(1, 8)  # randomise their speed
+
+def setup_sprites(screen_width, screen_height):
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.mouse.set_visible(False)  ##hides mouse
+
+    # player group
+    player = Player(screen_width, screen_height)
+    player_group = pygame.sprite.Group()
+    player_group.add(player)
+
+    # bullet group
+    bullet_group = pygame.sprite.Group()
+
+    # mob group
+    mob = pygame.sprite.Group()
+    mob_number = 5
+    for i in range(mob_number):  # no of mobs
+        m = Mob(screen_width, screen_height)
+        mob.add(m)
+
+
+
+    return player_group, mob, bullet_group, player, screen
+
+def draw_sprites(player_group, mob,bullet_group,player,screen):
+    # check to see if a bullet hits a mob
+    hits = pygame.sprite.groupcollide(mob, bullet_group, True,
+                                      True)  # bullet needs to run into mob and vice versa so 2 Trues
+    for hit in hits:  # this loop adds a mob if a mob dies
+        m = Mob()
+        mob.add(m)
+
+    # check to see if a mob hits player
+    hits = pygame.sprite.spritecollide(player, mob, False)  # hit list if player was hit
+    if hits:
+        game_running = False  # if player is hit by mob, loop stops and game exits
+
+    mob.update()
+    player_group.update()
+    mob.draw(screen)
+    bullet_group.draw(screen)
+    player_group.draw(screen)
+    bullet_group.update()
+
+    return player_group, mob,bullet_group
