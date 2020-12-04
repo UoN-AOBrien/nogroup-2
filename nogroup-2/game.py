5f4678894358d29ed6b935883089dda801a0faed
stypw01 <74611247+stypw01@users.noreply.github.com>
2020-12-04 19:01:55 +0000

    Merge

--- nogroup-2/game.py
+++ nogroup-2/game.py
@@ -39,9 +39,41 @@ def Game(screen):
     player_group.add(player)
     
     # Bullet group
-    bullet_group = pygame.sprite.Group()
+    #bullet group
+    rightbullet_group = pygame.sprite.Group()
+    leftbullet_group = pygame.sprite.Group()
+    downbullet_group = pygame.sprite.Group()
+    upbullet_group = pygame.sprite.Group()
+    
+    #mob group
+    mob = pygame.sprite.Group()
+    mob_number = 5
+    for i in range(mob_number):#no of mobs
+        m = eng.Mob()
+        mob.add(m)
+        
+    #heart group
+    heart = pygame.sprite.Group()
+    heart_number = 1
+    for i in range(heart_number):#no of hearts
+        h = eng.Heart()
+        heart.add(h)
+        
+    
+    starbullet = pygame.sprite.Group()
+    for i in range(1):#no of hearts
+        s = eng.StarBullet()
+        starbullet.add(s)  
+    
+        
+    frame_count = 0
+    frame_rate = 120
     
     
+    score = 0
+    kills = 0
+
+
     scroll_bg = [0, WIDTH]
     bg=[0, 0]
     for i in range(2):
@@ -80,16 +112,135 @@ def Game(screen):
             bg[1] = backgrounds[index]
         
         
+        """ TIMER AND SCORE """
+        total_seconds = frame_count // frame_rate
+        seconds = total_seconds % 60
+        if kills > 1 or kills == 1:
+            score = seconds + (kills*5)
+        else:
+            score = seconds
+        
+        
         """ PLAYER MECHANICS """
         if left_click:
-            bullet_group.add(player.create_bullet())
+            rightbullet_group.add(player.create_rightbullet())
+            
+        if player.life == 0:
+            game_running = False #if player is hit by mob, loop stops and game exits
+            eng.Shutdown()
+        else:
+            game_running = True  
         
+        """ COLLISIONS """
+        #check to see if a bullet hits a mob
+        mob_hit = pygame.sprite.groupcollide(mob, rightbullet_group, True, True)#bullet needs to run into mob and vice versa so 2 Trues
+        for hit in mob_hit: #this loop adds a mob if a mob dies
+            m = eng.Mob()
+            mob.add(m)
+            kills = kills + 1
+            
+        mob_hit = pygame.sprite.groupcollide(mob, leftbullet_group, True, True)#bullet needs to run into mob and vice versa so 2 Trues
+        for hit in mob_hit: #this loop adds a mob if a mob dies
+            m = eng.Mob()
+            mob.add(m)
+            kills = kills + 1
+            
+        mob_hit = pygame.sprite.groupcollide(mob, downbullet_group, True, True)#bullet needs to run into mob and vice versa so 2 Trues
+        for hit in mob_hit: #this loop adds a mob if a mob dies
+            m = eng.Mob()
+            mob.add(m)
+            kills = kills + 1
+            
+        mob_hit = pygame.sprite.groupcollide(mob, upbullet_group, True, True)#bullet needs to run into mob and vice versa so 2 Trues
+        for hit in mob_hit: #this loop adds a mob if a mob dies
+            m = eng.Mob()
+            mob.add(m)
+            kills = kills + 1
+            
+            
+        player_hit = pygame.sprite.spritecollide(player, mob, True) #hit list if player was hit
+        if player_hit:
+            player.life = player.life -1
+            mob.remove(m)
+            m = eng.Mob()
+            mob.add(m)
+            
+            
+            
+            
+        """ BOOSTS """
+        life_up = pygame.sprite.spritecollide(player, heart, True) #hit list if player was hit
+        if life_up:
+            player.life = player.life + 1
+            heart.remove(h)
+        
+        if player.life < 4:
+            if seconds%30 == 0:
+                heart.add(h)
+        
+    
+    
+            
+            
+        if seconds%30 == 0:
+            starbullet.add(s)  
+            
+        bullet_up = pygame.sprite.spritecollide(player, starbullet, True) #hit list if player was hit
+        if bullet_up:
+            leftbullet_group.add(player.create_leftbullet())
+            downbullet_group.add(player.create_downbullet())
+            rightbullet_group.add(player.create_rightbullet())
+            upbullet_group.add(player.create_upbullet())
+            starbullet.remove(s)
+        
+        
+        
+        
+
+        mob.update()
         player_group.update()
-        bullet_group.draw(screen)
+        heart.update()
+        starbullet.update()
+    
+        
+        
+        mob.draw(screen)
+        heart.draw(screen)
+        rightbullet_group.draw(screen)
+        leftbullet_group.draw(screen)
+        downbullet_group.draw(screen)
+        upbullet_group.draw(screen)
         player_group.draw(screen)
-        bullet_group.update()
+        starbullet.draw(screen)
+    
+    
+        
+        rightbullet_group.update()
+        leftbullet_group.update()
+        downbullet_group.update()
+        upbullet_group.update()
+            
+            
+       
         
         
-        # Refresh screen
-        clock.tick(FRAMERATE)
+    
+        
+        
+        myfont = pygame.font.SysFont('Comic Sans MS', 30)
+        lives = myfont.render(str(player.life), False, (153, 0, 153))
+        timer = myfont.render(str(seconds), False, (153, 0, 153))
+        score_total = myfont.render(str(score), False, (153, 0, 153))
+        kills_total = myfont.render(str(kills), False, (153, 0, 153))
+        
+      
+        screen.blit(lives,(0,0))
+        screen.blit(timer,(0,750))
+        screen.blit(score_total,(0,250))
+        screen.blit(kills_total,(750,250))
+       
+        
+        
+        frame_count = frame_count + 1
+        clock.tick(120)
         pygame.display.flip()
--- nogroup-2/utils/peggy.py
+++ nogroup-2/utils/peggy.py
@@ -14,9 +14,105 @@ import utils.harri as FuncHarri
 import utils.hongyuan as FuncHongyuan
 
 
-def bgmusic(bg_on):
-    pass
+class RightBullet(pygame.sprite.Sprite):
+    def __init__(self,pos_x,pos_y):
+        super().__init__()
+        self.image = pygame.Surface((50, 10))
+        self.image.fill((255,0,0))#colour
+        self.rect = self.image.get_rect(center = (pos_x,pos_y))
+        self.downwards = False
+
+        
+    def update(self):
+        self.rect.x += 5
+        if self.rect.x >= screen_width + 200: #if bullet goes too far to the right,
+            self.kill() #bullet will destroy itself to save memory and improve performance
+            
+class LeftBullet(pygame.sprite.Sprite):
+    def __init__(self,pos_x,pos_y):
+        super().__init__()
+        self.image = pygame.Surface((50, 10))
+        self.image.fill((255,0,0))#colour
+        self.rect = self.image.get_rect(center = (pos_x,pos_y))
+        self.downwards = False
+
+        
+    def update(self):
+        self.rect.x -= 5
+        if self.rect.x >= screen_width + 200: #if bullet goes too far to the right,
+            self.kill() #bullet will destroy itself to save memory and improve performance
+       
+        
+class DownBullet(pygame.sprite.Sprite):
+    def __init__(self,pos_x,pos_y):
+        super().__init__()
+        self.image = pygame.Surface((50, 10))
+        self.image.fill((255,0,0))#colour
+        self.rect = self.image.get_rect(center = (pos_x,pos_y))
+        self.downwards = False
+
+        
+    def update(self):
+        self.rect.y += 5
+        if self.rect.x >= screen_width + 200: #if bullet goes too far to the right,
+            self.kill() #bullet will destroy itself to save memory and improve performance
+       
+class UpBullet(pygame.sprite.Sprite):
+    def __init__(self,pos_x,pos_y):
+        super().__init__()
+        self.image = pygame.Surface((50, 10))
+        self.image.fill((255,0,0))#colour
+        self.rect = self.image.get_rect(center = (pos_x,pos_y))
+        self.downwards = False
+
+        
+    def update(self):
+        self.rect.y -= 5
+        if self.rect.x >= screen_width + 200: #if bullet goes too far to the right,
+            self.kill() #bullet will destroy itself to save memory and improve performance
+       
     
         
-    
+class Heart(pygame.sprite.Sprite): #spawn enemies
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,40))
+        self.image.fill((153, 0, 153))
+        self.rect = self.image.get_rect()
+        self.rect.y = random.randrange(screen_height - self.rect.height)#spanwns them on x axis outside of screen to the right
+        self.rect.x = random.randrange(700, screen_width)
+        self.speedx = random.randrange(1, 8)# randomise their speed
+
+    def update(self):
+        self.rect.x -= self.speedx #mobs go in left direction
+        if self.rect.x <= 0:
+            self.rect.y = random.randrange(screen_height - self.rect.height)  # spanwns them on x axis outside of screen to the right
+            self.rect.x = random.randrange(700, screen_width)  # spawn in a random place to the right of the screen
+            self.speedx = random.randrange(1,8)  # randomise their speed
+#currently new mobs dont spanwn, in the futuer after a mob dies, new one will spawn. need to do collision mehcanic first
+     
+        if self.rect.x >= screen_width + 200: #if bullet goes too far to the right,
+            self.kill()
+            
+            
+class StarBullet(pygame.sprite.Sprite): #spawn enemies
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30,40))
+        self.image.fill((255,255,255))
+        self.rect = self.image.get_rect()
+        self.rect.y = random.randrange(screen_height - self.rect.height)#spanwns them on x axis outside of screen to the right
+        self.rect.x = random.randrange(700, screen_width)
+        self.speedx = random.randrange(1, 8)# randomise their speed
+
+    def update(self):
+        self.rect.x -= self.speedx #mobs go in left direction
+        if self.rect.x <= 0:
+            self.rect.y = random.randrange(screen_height - self.rect.height)  # spanwns them on x axis outside of screen to the right
+            self.rect.x = random.randrange(700, screen_width)  # spawn in a random place to the right of the screen
+            self.speedx = random.randrange(1,8)  # randomise their speed
+#currently new mobs dont spanwn, in the futuer after a mob dies, new one will spawn. need to do collision mehcanic first
+     
+        if self.rect.x >= screen_width + 200: #if bullet goes too far to the right,
+            self.kill()
     
